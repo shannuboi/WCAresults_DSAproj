1ebb7a6569a4b57e528a10b2a21034228357f91a
shannu boi <shayan.khan.xyz@gmail.com>
2022-12-24 11:47:17 +0500

    Added and tested Pair class

---
+++ WCAresults/Pair.h
@@ -0,0 +1,17 @@
+#pragma once
+
+template <typename T1, typename T2>
+struct Pair
+{
+	T1 first;
+	T2 second;
+
+	bool operator==(const Pair& rhs)
+	{
+		return first == rhs.first && second == rhs.second;
+	}
+	bool operator!=(const Pair& rhs)
+	{
+		return !(*this == rhs);
+	}
+};
