c6be1b8365fe4833a1e0af05ebf8eb6867c8a67e
shannu boi <shayan.khan.xyz@gmail.com>
2022-12-28 23:07:40 +0500

    Fixed bug in linked list code

--- WCAresults/LinkedList.h
+++ WCAresults/LinkedList.h
@@ -17,6 +17,43 @@ private:
 		Node* next;
 	};
 public:
+	class ConstIterator // Public interface for a Node
+	{
+	public:
+		ConstIterator()
+			:
+			pNode(nullptr)
+		{}
+		ConstIterator(Node* in_pNode)
+			:
+			pNode(in_pNode)
+		{}
+		const datatype& operator*()
+		{
+			return pNode->data;
+		}
+		ConstIterator& operator++()
+		{
+			pNode = pNode->next;
+			return *this;
+		}
+		ConstIterator operator++(int)
+		{
+			Iterator nextIter(pNode->next);
+			++(*this);
+			return nextIter;
+		}
+		bool operator==(const ConstIterator& rhs)
+		{
+			return pNode == rhs.pNode;
+		}
+		bool operator!=(const ConstIterator& rhs)
+		{
+			return !(*this == rhs);
+		}
+	private:
+		Node* pNode;
+	};
 	class Iterator // Public interface for a Node
 	{
 	public:
@@ -51,42 +88,9 @@ public:
 		{
 			return !(*this == rhs);
 		}
-	private:
-		Node* pNode;
-	};
-	class ConstIterator // Public interface for a Node
-	{
-	public:
-		ConstIterator()
-			:
-			pNode(nullptr)
-		{}
-		ConstIterator(Node* in_pNode)
-			:
-			pNode(in_pNode)
-		{}
-		const datatype& operator*()
+		operator ConstIterator()
 		{
-			return pNode->data;
-		}
-		Iterator& operator++()
-		{
-			pNode = pNode->next;
-			return *this;
-		}
-		Iterator operator++(int)
-		{
-			Iterator nextIter(pNode->next);
-			++(*this);
-			return nextIter;
-		}
-		bool operator==(const Iterator& rhs)
-		{
-			return pNode == rhs.pNode;
-		}
-		bool operator!=(const Iterator& rhs)
-		{
-			return !(*this == rhs);
+			return ConstIterator(pNode);
 		}
 	private:
 		Node* pNode;
@@ -101,7 +105,20 @@ public:
 		DestroyList(head);
 	}
 	// Deep copy. (very slow since linkedlist does not have a end/last node ptr)
-	LinkedList& operator=(const LinkedList& rhs) 
+	LinkedList(const LinkedList<datatype>& source)
+		:
+		head(nullptr)
+	{
+		*this = source;
+	}
+	// Deep move
+	LinkedList(LinkedList<datatype>&& source)
+	{
+		head = source.head;
+		source.head = nullptr;
+	}
+	// Deep copy. (very slow since linkedlist does not have a end/last node ptr)
+	LinkedList& operator=(const LinkedList<datatype>& rhs)
 	{
 		DestroyList(head);
 
@@ -109,6 +126,13 @@ public:
 		{
 			PushBack(*iter);
 		}
+
+		return *this;
+	}
+	LinkedList& operator=(LinkedList<datatype>&& rhs)
+	{
+		head = rhs.head;
+		rhs.head = nullptr;
 	}
 	void PushFront(const datatype& value)
 	{
@@ -133,18 +157,23 @@ public:
 			cur->next = newnode;
 		}
 	}
+
+	// returns iterator holding head node
 	Iterator begin()
 	{
 		return Iterator(head);
 	}
+	// returns iterator holding to node after last (i.e. in this case nullptr)
 	Iterator end()
 	{
 		return Iterator();
 	}
+	// returns const iterator holding head node
 	ConstIterator begin() const
 	{
 		return ConstIterator(head);
 	}
+	// returns const iterator holding to node after last (i.e. in this case nullptr)
 	ConstIterator end() const
 	{
 		return Iterator();
