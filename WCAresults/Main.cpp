152e473c59de147aea8ac5b421f7d63aeebf4237
shannu boi <shayan.khan.xyz@gmail.com>
2022-12-31 17:24:39 +0500

    Updated and tested vector class

--- WCAresults/Main.cpp
+++ WCAresults/Main.cpp
@@ -1,7 +1,37 @@
-#include "Program.h"
+#include "Vector.h"
 #include <iostream>
+
+template <class T>
+void print(const my::Vector<T>& vec)
+{
+	for (int i = 0; i < vec.GetLength(); i++)
+	{
+		std::cout << vec[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
 int main()
 {
-	Program p;
+	my::Vector<int> vec(2);
+	std::cout << vec.isEmpty() << std::endl;
+	vec.Pushback(1);
+	print(vec);
+	vec.Pushback(2);
+	print(vec);
+	vec.Pushback(69);
+	print(vec);
+	vec.Pushback(4);
+	print(vec);
+	vec.Pushback(1);
+	print(vec);
+
+	my::Vector<int> newVec = vec;
+	newVec.Pushback(420);
+	print(vec);
+	print(newVec);
+
+	my::Vector<int> movedVec = std::move(vec);
+	print(vec);
+	print(movedVec);
 }
