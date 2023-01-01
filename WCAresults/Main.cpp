dba7d761017b4cde74a8fc78e6177c0f9de4bf2a
shannu boi <shayan.khan.xyz@gmail.com>
2023-01-01 23:13:00 +0500

    Added custom hashing function

--- WCAresults/Main.cpp
+++ WCAresults/Main.cpp
@@ -1,20 +1,7 @@
-#include "Hashmap.h"
+#include "Program.h"
 #include <iostream>
 
 int main()
 {
-	my::Hashmap<int, std::string> mymap(10);
-	mymap.Insert(1, "one");
-	mymap.Insert(2, "two");
-	mymap.Insert(7, "seven");
-	std::cout << mymap.Insert(4, "four") << mymap.Insert(1, "lol") << std::endl;
-
-	for (int i = 0; i < 10; i++)
-	{
-		auto val = mymap.Search(i);
-		if (val.second)
-			std::cout << val.first << std::endl;
-		else
-			std::cout << "NOPE" << std::endl;
-	}
+	Program p;
 }
