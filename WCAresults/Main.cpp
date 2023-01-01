fd3494ee8b158e4077df5600d65efe420ad12120
shannu boi <shayan.khan.xyz@gmail.com>
2023-01-01 18:01:42 +0500

    Updated and tested Hashmap class

--- WCAresults/Main.cpp
+++ WCAresults/Main.cpp
@@ -1,17 +1,20 @@
-#include "AVLtree.h"
+#include "Hashmap.h"
 #include <iostream>
 
 int main()
 {
-	auto printer = [](int val) {
-		std::cout << "|" << val << "|";
-	};
-	my::AVLtree<int> t;
-	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
-	t.Insert(50);
-	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
-	t.Insert(51);
-	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
-	t.Insert(52);
-	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
+	my::Hashmap<int, std::string> mymap(10);
+	mymap.Insert(1, "one");
+	mymap.Insert(2, "two");
+	mymap.Insert(7, "seven");
+	std::cout << mymap.Insert(4, "four") << mymap.Insert(1, "lol") << std::endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		auto val = mymap.Search(i);
+		if (val.second)
+			std::cout << val.first << std::endl;
+		else
+			std::cout << "NOPE" << std::endl;
+	}
 }
