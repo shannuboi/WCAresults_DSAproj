ce4320c53d0b9689c5a783e9f1c38aee64368865
shannu boi <shayan.khan.xyz@gmail.com>
2023-01-01 17:05:23 +0500

    AVLtree now has templatized comparer

--- WCAresults/Main.cpp
+++ WCAresults/Main.cpp
@@ -1,17 +1,17 @@
-#include "Program.h"
+#include "AVLtree.h"
 #include <iostream>
 
 int main()
 {
-	my::Vector<int> lol(5);
-	lol[0] = 10;
-	lol[1] = 110;
-	lol[2] = 100;
-	lol[3] = 60;
-	lol[4] = 0;
-
-	for (auto e : lol)
-	{
-		std::cout << e << " ";
-	}
+	auto printer = [](int val) {
+		std::cout << "|" << val << "|";
+	};
+	my::AVLtree<int, std::greater<int>> t;
+	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
+	t.Insert(50);
+	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
+	t.Insert(51);
+	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
+	t.Insert(52);
+	my::InOrderTriversal(t.begin(), t.end(), printer); std::cout << std::endl;
 }
