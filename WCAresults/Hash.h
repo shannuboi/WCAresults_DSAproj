89ffe354ae72cc7aef14075f76f7502b7b8ffafe
prithvirajmotwani <prithviraj182002@gmail.com>
2023-01-01 13:32:07 +0500

    Added hash

--- WCAresults/Hash.h
+++ WCAresults/Hash.h
@@ -1,1 +1,53 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "pair.h"
+
+template <typename keytype, typename valuetype>
+class HASH {
+public:
+
+    HASH(int cap);
+    int Hash(keytype key);
+    void Insert(keytype key, valuetype val);
+    valuetype Search(keytype key);
+
+private:
+
+    int capacity;
+    std::vector <Pair<keytype, valuetype>> hashtable;
+
+};
+
+template <typename keytype, typename valuetype>
+HASH<typename keytype, typename valuetype>::HASH(int cap)
+    :
+    capacity(cap),
+    hashtable(cap)
+{
+}
+
+
+template <typename keytype, typename valuetype>
+int HASH<typename keytype, typename valuetype>::Hash(keytype key) {
+    std::hash<keytype> hasher;
+    return hasher(key) % capacity;
+}
+
+
+template <typename keytype, typename valuetype>
+void HASH<typename keytype, typename valuetype>::Insert(keytype key, valuetype val) {
+    int index = Hash(key);
+    hashtable[index].first = key;
+    hashtable[index].second = val;
+}
+
+template <typename keytype, typename valuetype>
+valuetype HASH<typename keytype, typename valuetype>::Search(keytype key) {
+    int index = Hash(key);
+    if (hashtable[index].first == key)
+        return hashtable[index].second;
+    else
+        std::cout << "Value not found!!!!" << std::endl;
+}
